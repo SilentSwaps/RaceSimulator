496d5ada8f4e010fb0489255b172ae3cc3f4300a
SilentSwaps <leroyveldhoen@gmail.com>
2020-09-24 14:21:06 +0200

    Unit Test - Model_Competition_NextTrackShould. Level 3 Task 1

---
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+
+namespace ControllerTest
+{
+    [TestFixture]
+    class Model_Competition_NextTrackShould
+    {
+        private Competition _competition;
+
+        [SetUp]
+        public void SetUP()
+        {
+            _competition = new Competition();
+        }
+
+        [Test]
+        public void NextTrack_EmptyQueue_ReturnNull()
+        {
+            var result = _competition.NextTrack();
+            Assert.IsNull(result);
+        }
+    }
+}
